0dba49a28a98cddfb74cfbf84ed87b102a55c126
Equinoxx83 <schneiderdorian83@gmail.com>
2022-06-11 02:30:21 +0200

    premier commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+require("dotenv").config();
+
+client.on("ready", () => {
+    console.log("I'm ready !");
+});
+
+client.on("message", msg => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
